a63bfbaa4be110a11664b792e5510fb229b77507
Sagar Savaliya <sagarsavaliya2410@gmail.com>
2025-10-06 17:54:53 +0530

    build

--- lib/service/providers/api_and_auth.dart
+++ lib/service/providers/api_and_auth.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:dio/dio.dart';
 import 'package:untitled/service/api_service.dart';
 
 /// Provide ApiService instance — set baseUrl to your staging/production host
